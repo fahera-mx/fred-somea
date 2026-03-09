b09c561e1c62408e4bdc7d53a3fec6b3fff886e2
rhdzmota <code@rhdzmota.com>
2026-03-08 18:53:12 -0600

    SOM-3 Create a simple runnable command to facilitate local testing

--- fred-somea/src/main/fred/somea/cli/main.py
+++ fred-somea/src/main/fred/somea/cli/main.py
@@ -7,3 +7,15 @@ class CLI(AbstractCLI):
     def version(self):
         from fred.somea.version import version
         return version.value
+
+    @property
+    def social_media_catalog(self):
+        from fred.somea.sync.catalog import SyncCatalog
+
+        return [sm.name for sm in SyncCatalog]
+
+    def local_sync(self, social_media: str, **kwargs):
+        from fred.somea.sync.catalog import SyncCatalog
+
+        with SyncCatalog[social_media.upper()].auto(**kwargs) as sync:
+            return sync
